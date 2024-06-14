9c9fd96cf355c8066f7cd3d7799027c892959212
duri22 <myukonglove@gmail.com>
2024-06-14 10:51:09 +0900

    #3 사원 엔티티 추가

---
+++ src/main/java/com/errorCode/pandaOffice/employee/domain/entity/Job.java
@@ -0,0 +1,39 @@
+package com.errorCode.pandaOffice.employee.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="job")
+public class Job {
+    @Id
+    @Column(name="id")
+    private int id;
+    @Column
+    private String title;
+
+    protected Job(){}
+
+    public Job(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
