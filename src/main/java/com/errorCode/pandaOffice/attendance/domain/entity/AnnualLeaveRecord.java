83a9d68a4f1276892664fb6f61c691eb8a8734c4
lobster0202 <eodlf333@gmail.com>
2024-06-20 17:56:50 +0900

    "attendance-modify-entity"

--- src/main/java/com/errorCode/pandaOffice/attendance/domain/entity/AnnualLeaveRecord.java
+++ src/main/java/com/errorCode/pandaOffice/attendance/domain/entity/AnnualLeaveRecord.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
@@ -25,10 +26,13 @@ public class AnnualLeaveRecord {
     private int id;
 
     /* 연차 기록 변동일 */
-    private Date date;
+    private LocalDate date;
 
     /* 잔여 연차 */
     private double nowAmount;
+
+    /* 수량 (연차를 얼마나 썼는지, 받았는지에 대한 수량임)*/
+    private double amount;
 
     @ManyToOne
     @JoinColumn(name = "employee_id")
--- src/main/java/com/errorCode/pandaOffice/attendance/domain/entity/AttendanceRecord.java
+++ src/main/java/com/errorCode/pandaOffice/attendance/domain/entity/AttendanceRecord.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Date;
 
@@ -25,7 +26,7 @@ public class AttendanceRecord {
     private int id;
 
     /* 근무 기록 날짜 */
-    private Date date;
+    private LocalDate date;
 
     /* 근무 출근 시간 */
     private LocalTime checkInTime;
@@ -42,7 +43,7 @@ public class AttendanceRecord {
     /* 사번 */
     private Employee employee;
 
-    public AttendanceRecord(int id, Date date, LocalTime checkInTime,
+    public AttendanceRecord(int id, LocalDate date, LocalTime checkInTime,
                             LocalTime checkOutTime, LocalTime totalLateTime) {
         this.id = id;
         this.date = date;
