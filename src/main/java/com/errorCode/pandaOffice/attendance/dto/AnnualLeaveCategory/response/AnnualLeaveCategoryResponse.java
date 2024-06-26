e790d79b764e8974e2ec8e92ebe60b37a8aa3e2d
lobster0202 <eodlf333@gmail.com>
2024-06-26 09:39:33 +0900

    "Attendance Status Page Finish"

--- src/main/java/com/errorCode/pandaOffice/attendance/dto/AnnualLeaveCategory/response/AnnualLeaveCategoryResponse.java
+++ src/main/java/com/errorCode/pandaOffice/attendance/dto/AnnualLeaveCategory/response/AnnualLeaveCategoryResponse.java
@@ -2,6 +2,8 @@ package com.errorCode.pandaOffice.attendance.dto.AnnualLeaveCategory.response;
 
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @ToString
@@ -10,9 +12,26 @@ import lombok.*;
 public class AnnualLeaveCategoryResponse {
 
     /* 연차 분류 코드 */
-    private int id;
+    private int categoryId;
 
     /* 연차 분류 이름 */
-    private String name;
+    private String categoryName;
+
+    /* 연차 기록 코드 */
+    private int recordId;
+
+    /* 연차 기록 날짜 */
+    private LocalDate recordDate;
+
+    /* 잔여 연차 */
+    private double nowAmount;
+
+    /* 연차 변동 수량 */
+    private double amount;
+
+    /* 신청 서류 코드 */
+    private int approvalDocumentId;
+
+
 
 }
