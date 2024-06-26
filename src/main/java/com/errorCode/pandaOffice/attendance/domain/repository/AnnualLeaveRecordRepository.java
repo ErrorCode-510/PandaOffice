b2208b2212228aeb9cf45dd8820c276bc4dc75bd
lobster0202 <eodlf333@gmail.com>
2024-06-26 16:46:25 +0900

    "Annual Leave Record Page Finish"

--- src/main/java/com/errorCode/pandaOffice/attendance/domain/repository/AnnualLeaveRecordRepository.java
+++ src/main/java/com/errorCode/pandaOffice/attendance/domain/repository/AnnualLeaveRecordRepository.java
@@ -5,14 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AnnualLeaveRecordRepository extends JpaRepository<AnnualLeaveRecord, Integer> {
-    List<AnnualLeaveRecord> findByEmployee_EmployeeId(int employeeId);
 
     @Query("SELECT alr " +
             "FROM AnnualLeaveRecord as alr " +
             "JOIN alr.annualLeaveCategory as ac " +
             "WHERE alr.employee.employeeId  = :employeeId")
     List<AnnualLeaveRecord> findByEmployeeIdWithCategory(@Param("employeeId") int employeeId);
+
+    List<AnnualLeaveRecord> findByEmployee_EmployeeIdAndDateBetween(int employeeId, LocalDate startDate, LocalDate endDate);
 }
--- src/main/java/com/errorCode/pandaOffice/attendance/dto/AnnualLeaveCategory/response/AnnualLeaveCategoryResponse.java
+++ src/main/java/com/errorCode/pandaOffice/attendance/dto/AnnualLeaveCategory/response/AnnualLeaveCategoryResponse.java
@@ -12,25 +12,13 @@ import java.time.LocalDate;
 public class AnnualLeaveCategoryResponse {
 
     /* 연차 분류 코드 */
-    private int categoryId;
+    private int id;
 
     /* 연차 분류 이름 */
-    private String categoryName;
+    private String name;
 
-    /* 연차 기록 코드 */
-    private int recordId;
-
-    /* 연차 기록 날짜 */
-    private LocalDate recordDate;
-
-    /* 잔여 연차 */
-    private double nowAmount;
-
-    /* 연차 변동 수량 */
-    private double amount;
-
-    /* 신청 서류 코드 */
-    private int approvalDocumentId;
+    /* 연차 분류의 타입 */
+    private String type;
 
 
 
